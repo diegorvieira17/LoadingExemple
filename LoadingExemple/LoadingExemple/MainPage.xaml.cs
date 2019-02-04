72e7c082b78317af0f6da55b61c51d237dcc14c8
Diego Vieira <diegorvieira17@outlook.com>
2019-02-04 15:17:56 -0200

    LoadingExemple

---
+++ LoadingExemple/LoadingExemple/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoadingExemple
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            IsLoading = false;
+            BindingContext = this;
+        }
+
+        public async void BtnLoading_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                aparece();
+
+                //Chame sua função aqui
+                await Task.Delay(4000);
+
+
+                some();
+            }
+            catch (Exception ex)
+            {
+                some();
+                if (ex != null)
+                {
+                    //Trate seu erro aqui
+                }
+            }
+        }
+
+        public async void aparece()
+        {
+            IsLoading = true;
+        }
+
+        public async void some()
+        {
+            IsLoading = false;
+        }
+
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get
+            {
+                return this.isLoading;
+            }
+            set
+            {
+                this.isLoading = value;
+                RaisePropertyChanged("IsLoading");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
